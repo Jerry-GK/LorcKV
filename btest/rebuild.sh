c5a3a9cdc3410523a7734110fe92378113885301
Jerry-GK <22421086@zju.edu.cn>
2025-04-17 10:58:58 +0000

    modify rebuild.sh

--- btest/rebuild.sh
+++ btest/rebuild.sh
@@ -23,6 +23,11 @@ mkdir -p "$build_dir"
 
 # 进入构建目录并运行 CMake 和 Make
 cd "$build_dir" || exit 1
-cmake -DCMAKE_BUILD_TYPE=Release -DWITH_GFLAGS=1 -DWITH_SNAPPY=1 ..
-make -j
+sudo cmake -DWITH_JEMALLOC=0 -DWITH_SNAPPY=1 -DWITH_LZ4=1 -DWITH_ZLIB=1 -DCMAKE_BUILD_TYPE=Release -DWITH_GFLAGS=1 -DWITH_JNI=1 \
+         -DJAVA_HOME=/home/tx/jdk1.8.0_401 \
+         -DJAVA_INCLUDE_PATH=/home/tx/jdk1.8.0_401/include \
+         -DJAVA_INCLUDE_PATH2=/home/tx/jdk1.8.0_401/include/linux \
+         -DJAVA_JVM_LIBRARY=/home/tx/jdk1.8.0_401/jre/lib/amd64/server/libjvm.so \
+         ..
+sudo make -j
 cd ..
